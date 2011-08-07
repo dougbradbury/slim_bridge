4e00d76b08ef3afd5da2af197ed40089e35ba79a
dougbradbury <doug@8thlight.com>
2011-08-06 23:50:19 -0500

    clean up serial

--- lib/run_serial_slim_bridge.rb
+++ lib/run_serial_slim_bridge.rb
@@ -1,11 +1,5 @@
 require 'serialport'
 require 'slim_bridge_server'
 
-
-def run_tcp(fitnese_port, remote_host, remote_port)
-  run_server(port, TcpSocket.new(remote_host, remote_port))
-end
-
-
-  device, baud_rate, port = *ARGV
-  SlimBridgeServer::run(port, SerialPort.new(device, baud_rate.to_i))
+device, baud_rate, port = *ARGV[0..2]
+SlimBridgeServer::run(port, SerialPort.new(device, baud_rate.to_i))
